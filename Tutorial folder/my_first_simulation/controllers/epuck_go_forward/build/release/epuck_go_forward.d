a7cfaf9aa8c2478428e3a6ac5f501c7d25419e2b
Qibal2407 <119699912+Qibal2407@users.noreply.github.com>
2023-01-21 01:50:05 +0700

    Add files via upload

---
+++ Tutorial folder/my_first_simulation/controllers/epuck_go_forward/build/release/epuck_go_forward.d
@@ -0,0 +1,5 @@
+build/release/epuck_go_forward.o: epuck_go_forward.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h
